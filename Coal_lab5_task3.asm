d711130d6b8049aac08c97f7631e8b3bc47795a1
EmaanAhmed05 <emaanahmed662@gmail.com>
2025-12-01 07:51:30 -0800

    Add files via upload

---
+++ Coal_lab5_task3.asm
@@ -0,0 +1,30 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt 
+
+org 100h
+
+mov ah,1
+int 21h
+mov bl,al
+
+mov ah,1
+int 21h
+mov bh,al
+         
+mov ah,2
+mov dl,bh
+int 21h
+
+mov ah,2
+mov dl,bl
+int 21h      
+
+mov ah, 4Ch
+int 21
+
+ret
+
+
+
+
